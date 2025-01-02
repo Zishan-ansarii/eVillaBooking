9fd0b33e711f935ce85abf2363f327eed5bc40e1
Zishan-ansarii <zishanansari.dev@gmail.com>
2025-01-02 22:30:42 +0530

    managing and adding new villa list

---
+++ eVillaBooking.Presentation/Controllers/VillaController.cs
@@ -0,0 +1,36 @@
+﻿using eVillaBooking.Domain.Entities;
+using eVillaBooking.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eVillaBooking.Presentation.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+
+        {
+            return View(_db.Villas.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Villas.Add(villa);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(villa);
+        }
+    }
+}
