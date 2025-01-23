64d27ca8f8b5a4502649d2b038c03960e8f1aa61
Zishan-ansarii <zishanansari.dev@gmail.com>
2025-01-23 16:43:10 +0530

    Generic Repository implemented

--- eVillaBooking.Application/Common/Interfaces/IVillaRepository.cs
+++ eVillaBooking.Application/Common/Interfaces/IVillaRepository.cs
@@ -1,19 +1,12 @@
-﻿using eVillaBooking.Domain.Entities;
+﻿using eVillaBooking.Application.Common.Interfaces;
+using eVillaBooking.Domain.Entities;
 using System.Linq.Expressions;
 
 namespace eVillaBooking.Application.Common.Interfaces
 {
-    public interface IVillaRepository
+    public interface IVillaRepository : IRepository<Villa> 
     {
-        IEnumerable<Villa> GetAll(Expression<Func<Villa,bool>>? filter=null,string? includeProperties=null);
-
-        Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties=null);
-
-        void Add(Villa villa);
         void Update(Villa villa);
-        void Remove(Villa villa);
         void save();
-
-
     }
 }
--- eVillaBooking.Infrastructure/Repository/VillaRepository.cs
+++ eVillaBooking.Infrastructure/Repository/VillaRepository.cs
@@ -6,17 +6,12 @@ using System.Linq.Expressions;
 
 namespace eVillaBooking.Infrastructure.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa> ,IVillaRepository
     {
         private readonly ApplicationDbContext _db;
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
-        }
-
-        public void Add(Villa villa)
-        {
-            _db.Villas.Add(villa);
         }
 
         public void Update(Villa villa)
@@ -24,50 +19,10 @@ namespace eVillaBooking.Infrastructure.Repository
             _db.Villas.Update(villa);
         }
 
-        public void Remove(Villa villa)
-        {
-            _db.Villas.Remove(villa);
-        }
-
         public void save()
         {
             _db.SaveChanges();
         }
-
-        public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Villas;
-
-            query = query.Where(filter);
-
-            if (includeProperties is not null)
-            {
-                foreach (var IncludeProperty in includeProperties.Split(new char[] { ',', ' ', '-', '_' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(IncludeProperty);
-                }
-            }
-            return query.FirstOrDefault();
-        }
-
-        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _db.Villas;
-            if (filter is not null)
-            {
-                query = query.Where(filter);
-            }
-
-            if (includeProperties is not null)
-            {
-                foreach (var IncludeProperty in includeProperties.Split(new char[] {',',' ','-','_' }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    query = query.Include(IncludeProperty);
-                }
-            }
-            return query.ToList();
-        }
-
 
     }
 }
