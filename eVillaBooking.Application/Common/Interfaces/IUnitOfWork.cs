2148d97ed07f57ef165fb338cf40e7d1d43cb3e1
Zishan-ansarii <zishanansari.dev@gmail.com>
2025-03-13 07:18:19 +0530

    feat : Added - ApplicationUser repo - Booking Confirmation view and - booking functionality done

--- eVillaBooking.Application/Common/Interfaces/IUnitOfWork.cs
+++ eVillaBooking.Application/Common/Interfaces/IUnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eVillaBooking.Application.Common.Interfaces
+﻿namespace eVillaBooking.Application.Common.Interfaces
 {
     public interface IUnitOfWork
     {
@@ -12,6 +6,7 @@ namespace eVillaBooking.Application.Common.Interfaces
         public IVillaNumberRepository VillaNumberRepositoryUOW { get; }
         public IAmenityRepository AmenityRepositoryUOW { get; }
         public IBookingRepository BookingRepositoryUOW { get; }
+        public IApplicationUserRepository ApplicationUserRepositoryUOW { get; }
 
         void Save();
 
--- eVillaBooking.Presentation/Controllers/BookingController.cs
+++ eVillaBooking.Presentation/Controllers/BookingController.cs
@@ -1,6 +1,9 @@
 ﻿using eVillaBooking.Application.Common.Interfaces;
+using eVillaBooking.Application.Utility;
 using eVillaBooking.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace eVillaBooking.Presentation.Controllers
 {
@@ -14,19 +17,47 @@ namespace eVillaBooking.Presentation.Controllers
             _unitOfWork = unitOfWork;
         }
 
+        [Authorize]
         public IActionResult FinalizeBooking(DateOnly checkInDate, int nights, int villaId)
-        {    
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity!;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUserRepositoryUOW.Get(au => au.Id == userId);
             Booking booking = new()
             {
                 CheckInDate = checkInDate,
                 Nights = nights,
                 VillaId = villaId,
                 CheckOutDate = checkInDate.AddDays(nights),
-                Villa = _unitOfWork.VillaRepositoryUOW.Get(v => v.Id == villaId,includeProperties:"AmenityList"),
+                Villa = _unitOfWork.VillaRepositoryUOW.Get(v => v.Id == villaId, includeProperties: "AmenityList"),
+                Email = applicationUser.Email!,
+                Name = applicationUser.Name,
+                Phone = applicationUser.PhoneNumber,
+                UserId = userId
             };
+
 
             booking.TotalCost = booking.Villa.Price * nights;
             return View(booking);
+        }
+
+        [Authorize,HttpPost]
+        public IActionResult FinalizeBooking(Booking booking)
+        {
+            Villa villa = _unitOfWork.VillaRepositoryUOW.Get(v => v.Id == booking.VillaId);
+            booking.TotalCost = villa.Price * booking.Nights;
+            
+            booking.BookingDate = DateTime.Now;
+            booking.Status = StaticDetails.StatusApproved;
+
+            _unitOfWork.BookingRepositoryUOW.Add(booking);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(BookingConfirmation), new { bookingId=booking.Id });
+        }
+
+        public IActionResult BookingConfirmation(int bookingId)
+        {
+            return View(bookingId);
         }
     }
 }
